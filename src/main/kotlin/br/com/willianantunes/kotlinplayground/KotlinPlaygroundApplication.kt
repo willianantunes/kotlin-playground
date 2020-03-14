542ff4c8e0b6e315a9f31a7e8eca86bd851e07bb
Willian Antunes <willian.lima.antunes@gmail.com>
2020-03-14 11:22:35 -0300

    Initial commit
    Boilerplate project baked on Spring Initializr:
    - https://start.spring.io/

---
+++ src/main/kotlin/br/com/willianantunes/kotlinplayground/KotlinPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package br.com.willianantunes.kotlinplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/willianantunes/kotlinplayground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,12 @@
+package br.com.willianantunes.kotlinplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
