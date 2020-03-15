101d42fab361a91ca941ad26a3e90a17aa53fd51
Willian Antunes <willian.lima.antunes@gmail.com>
2020-03-15 07:09:49 -0300

    Setup for Spring Boot Actuator
    Know more at:
    - http://bit.ly/2TUxqDz

--- src/test/kotlin/br/com/willianantunes/kotlinplayground/KotlinPlaygroundApplicationTests.kt
+++ src/test/kotlin/br/com/willianantunes/kotlinplayground/KotlinPlaygroundApplicationTests.kt
@@ -1,11 +1,26 @@
 package br.com.willianantunes.kotlinplayground
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 
-@SpringBootTest
-class KotlinPlaygroundApplicationTests {
+val REQUEST_PATH_ACTUATOR = "/actuator/health"
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class KotlinPlaygroundApplicationTests @Autowired constructor(val restTemplate: TestRestTemplate) {
     @Test
-    fun contextLoads() {
+    fun `Should load Spring Boot context`() {
+    }
+
+    @Test
+    fun `Should configure actuator`() {
+        val entity = restTemplate.getForEntity(REQUEST_PATH_ACTUATOR, String::class.java)
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.headers.contentType.toString()).isEqualTo(MediaType.APPLICATION_JSON_VALUE)
+        assertThat(entity.body).isEqualTo("""{"status":"UP"}""")
     }
 }
