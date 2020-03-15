5fa23a55286872323584a6a84924979e49f211f8
Willian Antunes <willian.lima.antunes@gmail.com>
2020-03-15 07:19:15 -0300

    To make tests run. This should be fixed soon

--- src/test/kotlin/br/com/willianantunes/kotlinplayground/rest/GreetingControllerTest.kt
+++ src/test/kotlin/br/com/willianantunes/kotlinplayground/rest/GreetingControllerTest.kt
@@ -1,9 +1,12 @@
 package br.com.willianantunes.kotlinplayground.rest
 
 
+import br.com.willianantunes.kotlinplayground.services.ArticleRepository
+import br.com.willianantunes.kotlinplayground.services.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -15,6 +18,12 @@ val REQUEST_PATH_GREETING = "/greeting"
 
 @WebMvcTest
 class GreetingControllerTest(@Autowired val mockMvc: MockMvc) {
+    // This is a problem. I had to put these two MockBeans because SB is requiring beans for these two repositories
+    @MockBean
+    private lateinit var userRepository: UserRepository
+    @MockBean
+    private lateinit var articleRepository: ArticleRepository
+
     @Test
     fun `Should return hello word with ID 1 given this is the first request to the endpoint`() {
         mockMvc.perform(get(REQUEST_PATH_GREETING))
