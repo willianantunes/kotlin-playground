e365f5bb1316e0a45daf397c7829a2b9889b7d33
Willian Antunes <willian.lima.antunes@gmail.com>
2020-03-22 18:25:46 -0300

    Assertion on the duration of execution and use of dedicated DC service
    Dispatchers.IO is the correct guy for ticketService purpose

--- src/test/kotlin/br/com/willianantunes/kotlinplayground/services/StockServiceIT.kt
+++ src/test/kotlin/br/com/willianantunes/kotlinplayground/services/StockServiceIT.kt
@@ -1,16 +1,47 @@
 package br.com.willianantunes.kotlinplayground.services
 
 import br.com.willianantunes.kotlinplayground.timeIt
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.containers.DockerComposeContainer
+import java.io.File
+import java.time.Duration
 
 
 @SpringBootTest
 class StockServiceIT(@Autowired val ticketService: StockService) {
+    companion object {
+        private val instance: KDockerComposeContainer by lazy { defineDockerCompose() }
+
+        class KDockerComposeContainer(file: File) : DockerComposeContainer<KDockerComposeContainer>(file)
+
+        private fun defineDockerCompose() = KDockerComposeContainer(File("docker-compose.yml"))
+
+        @BeforeAll
+        @JvmStatic
+        internal fun beforeAll() {
+            with(instance) {
+                withServices("ticker-service")
+                withLocalCompose(true)
+            }.also {
+                it.start()
+            }
+        }
+
+        @AfterAll
+        @JvmStatic
+        internal fun afterAll() {
+            instance.stop()
+        }
+    }
+
     @Test
     fun `Should retrieve stock price for GOOG, AMZN and MSFT given SYNC one consult at a time`() {
         val duration = timeIt {
@@ -24,15 +55,16 @@ class StockServiceIT(@Autowired val ticketService: StockService) {
         }
 
         println("SYNC - It took $duration")
+        assertThat(duration).isGreaterThan(Duration.ofSeconds(3))
     }
 
     @Test
     fun `Should retrieve stock price for GOOG, AMZN and MSFT given ASYNC consult for each`() {
         val duration = timeIt {
             runBlocking {
-                val priceForGoogle = async { ticketService.getStockPrice("GOOG") }
-                val priceForAmazon = async { ticketService.getStockPrice("AMZN") }
-                val priceForMicrosoft = async { ticketService.getStockPrice("MSFT") }
+                val priceForGoogle = async(Dispatchers.IO) { ticketService.getStockPrice("GOOG") }
+                val priceForAmazon = async(Dispatchers.IO) { ticketService.getStockPrice("AMZN") }
+                val priceForMicrosoft = async(Dispatchers.IO) { ticketService.getStockPrice("MSFT") }
 
                 assertThat(priceForGoogle.await()).isEqualTo("1168.19")
                 assertThat(priceForAmazon.await()).isEqualTo("1902.42")
@@ -41,5 +73,6 @@ class StockServiceIT(@Autowired val ticketService: StockService) {
         }
 
         println("ASYNC - It took $duration")
+        assertThat(duration).isLessThan(Duration.ofSeconds(2))
     }
 }
